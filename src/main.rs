5904da03960eb59d44725f1112d069681e0f9971
steve.cooper <steve.cooper@anaplan.com>
2023-06-21 12:17:57 +0100

    Commit Wed Jun 21 12:17:57 BST 2023

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,16 @@ mod webserver;
 use crate::webserver::SocketAddrHelper;
 use anyhow::Result;
 use kube::Client;
+use std::net::{SocketAddr, SocketAddrV4};
+use std::str::FromStr;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // connect to the current context and list nodes;
     let client = Client::try_default().await?;
 
-    let addr = SocketAddrHelper::find_open_port();
+    // let addr = SocketAddrHelper::find_open_port();
+    let addr = SocketAddr::V4(SocketAddrV4::from_str("0.0.0.0:3001").unwrap());
     println!("Start the server: http://{}", addr);
     let routes = router::routes(client.clone());
     let server = webserver::GestaltServer::new().with(routes);
